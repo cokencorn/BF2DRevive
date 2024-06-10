16dd13d0f742daa863fd674ea0fae41be8e3d683
Altay <altayadademir@gmail.com>
2024-06-10 15:08:55 +0300

    Updated codebase to Python 3

--- Servers/ServerListRegister.py
+++ Servers/ServerListRegister.py
@@ -107,8 +107,10 @@ class ServerListRegister(Thread):
     def check_servers(self):
         checker = threading.Timer(30, self.check_servers)
         checker.start()
-        for address, server in self.registered_servers.items():
+        for address, server in self.registered_servers.copy().items():
+            print('checking server')
             if not server.is_active():
+                print('removing server')
                 self.server_disconnect(address)
 
     def debug(self, string):
